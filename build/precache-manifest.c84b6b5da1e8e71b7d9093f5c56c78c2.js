99015b85bd201b46f99fb7a2911a12b70b9477a0
Bhanu Kiran T <bhanukirant99@gmail.com>
2020-09-08 04:27:56 +0530

    correcting the errors

---
+++ build/precache-manifest.c84b6b5da1e8e71b7d9093f5c56c78c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f783d45b614e3d73175be774b071162",
+    "url": "/bhanukirant99/my-react-portfolioindex.html"
+  },
+  {
+    "revision": "1b5d0767c11e1f5d7293",
+    "url": "/bhanukirant99/my-react-portfoliostatic/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/bhanukirant99/my-react-portfoliostatic/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/bhanukirant99/my-react-portfoliostatic/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b5d0767c11e1f5d7293",
+    "url": "/bhanukirant99/my-react-portfoliostatic/js/main.326bee02.chunk.js"
+  },
+  {
+    "revision": "b9869eda7c686687a2e5",
+    "url": "/bhanukirant99/my-react-portfoliostatic/js/runtime-main.9c2ab909.js"
+  }
+]);
